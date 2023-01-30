06ed81b4817f7a56dfde0b4eea12ae5e2cca9fd2
Devansh s joshi <123493748+Devanshjoshi5121@users.noreply.github.com>
2023-01-30 23:28:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var btc = documen.getElementById("bitcoin");
+var eth = documen.getElementById("ethereum");
+var doge = documen.getElementById("dogecoin");
+
+var settings = {
+    "async": true,
+    "scrossDomain": true,
+    "url": "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd",
+    "method": "GET",
+    "headers": {}
+}
+$.ajax(settings).done(function(response){
+    btc.innerHTML = response.bitcoin.usd;
+    eth.innerHTML = response.ethereum.usd;
+    doge.innerHTML = response.dogecoin.usd;
+});
